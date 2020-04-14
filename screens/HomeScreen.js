d757d100a0d8c8fa09be01e32617fd92985c705d
Kona Tess <konatess@gmail.com>
2020-04-13 18:58:51 -0700

    Still messing with list basics for understanding.

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -21,7 +21,12 @@ export default function HomeScreen() {
             { key: 'Dan' },
             { key: 'Dominic' },
             { key: 'Jackson' },
-            { key: 'James' }
+            { key: 'James' },
+            { key: 'Joel' },
+            { key: 'John' },
+            { key: 'Jillian' },
+            { key: 'Jimmy' },
+            { key: 'Julie' }
           ]}
           renderItem={({ item }) => <Text style={styles.item}>{item.key}</Text>}
       />
