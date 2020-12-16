12a2f22393e2c7afc5c02457d38bed9de8f86272
Theresa Benkman <konatess@gmail.com>
2020-12-16 09:25:46 -0800

    Updated button icons after some changes were released in icon naming.

--- constants/ButtonClass.js
+++ constants/ButtonClass.js
@@ -7,29 +7,29 @@ class ButtonObj {
         this._iconName= i; 
     }
 };
-const settings= new ButtonObj('', Colors.settings, 'md-settings');
+const settings= new ButtonObj('', Colors.settings, 'md-settings-sharp');
 const home= new ButtonObj('', Colors.home, 'md-home');
-const order= new ButtonObj('', Colors.order, 'md-reorder');
-const save= new ButtonObj('', Colors.save, 'md-save');
-const trash= new ButtonObj('', Colors.delete, 'md-trash');
-const trash2= new ButtonObj('', Colors.delete, 'md-trash');
-const create= new ButtonObj('', Colors.create, 'md-add');
-const edit= new ButtonObj('', Colors.edit, 'md-create');
-const set= new ButtonObj('', Colors.edit, 'md-create');
+const order= new ButtonObj('', Colors.order, 'md-reorder-three');
+const save= new ButtonObj('', Colors.save, 'save-sharp');
+const trash= new ButtonObj('', Colors.delete, 'trash-sharp');
+const trash2= new ButtonObj('', Colors.delete, 'trash-sharp');
+const create= new ButtonObj('', Colors.create, 'md-add-sharp');
+const edit= new ButtonObj('', Colors.edit, 'create-outline');
+const set= new ButtonObj('', Colors.edit, 'create-outline');
 const cancel= new ButtonObj('', Colors.cancel, 'md-close');
 const cancel2= new ButtonObj('', Colors.cancel, 'md-close');
 const done= new ButtonObj('', Colors.done, 'md-checkmark');
 const okay= new ButtonObj('', Colors.done, 'md-checkmark');
 const darkMode= new ButtonObj('', Colors.done, 'md-contrast');
-const language= new ButtonObj('', Colors.done, 'md-globe');
+const language= new ButtonObj('', Colors.done, 'language');
 const dateFormat= new ButtonObj('', Colors.done, 'md-calendar');
 const notifications= new ButtonObj('', Colors.done, 'md-notifications');
-const freq= new ButtonObj('', Colors.done, 'md-notifications');
-const time= new ButtonObj('', Colors.done, 'md-notifications');
-const unit= new ButtonObj('', Colors.done, 'md-list');
-const editUnit= new ButtonObj('', Colors.edit, 'md-add');
-const deleteAll= new ButtonObj('', Colors.done, 'md-trash');
-const feedback= new ButtonObj('', Colors.done, 'md-chatbubbles');
+const freq= new ButtonObj('', Colors.done, 'repeat');
+const time= new ButtonObj('', Colors.done, 'time-outline');
+const unit= new ButtonObj('', Colors.done, 'list');
+const editUnit= new ButtonObj('', Colors.edit, 'md-add-sharp');
+const deleteAll= new ButtonObj('', Colors.done, 'trash-sharp');
+const feedback= new ButtonObj('', Colors.done, 'chatbubble');
 
 export default {
     settings: settings,
