6cc882cc520793dec585e5c0fa0e9a1c02d7efa2
Theresa Benkman <konatess@gmail.com>
2024-05-03 00:27:27 -0700

    Added code to use production ids in production by platform, and test ids in test for banner ads.

--- components/AdBar.js
+++ components/AdBar.js
@@ -1,10 +1,15 @@
 import * as React from 'react';
+import { Platform } from 'react-native';
 
 import { BannerAd, BannerAdSize, TestIds } from 'react-native-google-mobile-ads';
 
+const prodAdId = Platform.OS === 'android' ? "ca-app-pub-4985866248559217~8464918921" : "ca-app-pub-4985866248559217~9454900107"
+
+const adUnitId = __DEV__ ? TestIds.BANNER : prodAdId;
+
 export default function MyAdBar() {
     return <BannerAd
-        unitId={TestIds.BANNER}
+        unitId={adUnitId}
         sizes={[BannerAdSize.FULL_BANNER]}
         requestOptions={{
             requestNonPersonalizedAdsOnly: true,
